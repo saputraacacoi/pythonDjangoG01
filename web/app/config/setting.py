65322663727023133250145b0471efd70a6dfd1d
saputra <saputrat27@gmail.com>
2017-11-06 11:11:25 +0800

    Based

---
+++ web/app/config/setting.py
@@ -0,0 +1,46 @@
+import os, django
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+SECRET_KEY = 'e^r=(#ble-w)ji@_!a3otik+d=ht&#$=$aqwzmhx^on3c$e$ev'
+DEBUG = True
+ALLOWED_HOSTS = [
+
+]
+
+PROJECT_APPS = [
+
+]
+
+REQUIRED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+INSTALLED_APPS = REQUIRED_APPS + PROJECT_APPS
+
+ROOT_URLCONF = 'app.urls'
+WSGI_APPLICATION = 'app.wsgi.application'
+
+
+from app.config.auth import *
+from app.config.database import *
+from app.config.international import *
+from app.config.static import *
+django.setup()
+from app.config.template import *
